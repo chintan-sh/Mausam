6258604a482073b6a5e8ce1b248a662b3e4168e3
chintan <chintan.shah104@gmail.com>
2017-06-13 18:43:07 -0400

    Example for arrow functions

---
+++ example/es-arrow-functions.js
@@ -0,0 +1,31 @@
+/**
+ * Created by chintan on 6/13/17.
+ */
+
+var names = ['Mike', 'Steven', 'Elon'];
+//
+// names.forEach(function(name){
+//     console.log("Anon function output :- " + name);
+// });
+//
+// console.log("-------------------------------------");
+// names.forEach((name) => {
+//     console.log("Arrow function output :- " + name);
+// });
+//
+// console.log("-------------------------------------");
+// names.forEach((name)=> console.log("Single line arrow function output :- " + name)) ;
+//
+// var returnMe = (name) => name + '!' ;
+// console.log(returnMe("Sam"));
+
+var person = {
+    name : 'Masoom',
+    greet : function(){
+        names.forEach((name) => {
+            console.log(this.name + " says hx3 to " + name);
+        });
+    }
+}
+
+person.greet();
