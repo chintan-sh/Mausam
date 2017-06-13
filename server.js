4820a665ed2a588f46380986699b42dc47647c00
chintan <chintan.shah104@gmail.com>
2017-06-13 19:50:38 -0400

    Support for Heroku

--- server.js
+++ server.js
@@ -4,10 +4,19 @@
 var express = require('express');
 
 // Create our app
- var app = express();
+var app = express();
+const PORT = process.env.PORT || 3000; // making change for heroku as it decides the port
+
+app.use( function(req, res, next){
+    if(req.headers['x-forwarded-proto'] === 'http'){
+        next();
+    }else{
+        res.redirect("http://" + req.hostname + req.url);
+    }
+});
 
 app.use(express.static('./'));
 
-app.listen(3000,function(){
-    console.log('Server is up and running on :3000')
+app.listen(PORT,function(){
+    console.log('Server is up and running on :' + PORT)
 });
