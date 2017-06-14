9800d0f211f8b4031405a1a5b2c6b0a52a5b1aae
chintan <chintan.shah104@gmail.com>
2017-06-13 20:02:17 -0400

    Updated server startup script

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ var app = express();
 const PORT = process.env.PORT || 3000; // making change for heroku as it decides the port
 
 app.use( function(req, res, next){
-    if(req.headers['x-forwarded-proto'] === 'http'){
-        next();
-    }else{
+    if(req.headers['x-forwarded-proto'] === 'https'){
         res.redirect("http://" + req.hostname + req.url);
+    }else{
+        next();
     }
 });
 
