812a0ea7ca4b0b855d7d6d2fd205a5378420f491
chintan <chintan.shah104@gmail.com>
2017-06-19 21:24:42 -0400

    Fixed error modal bug

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react'); // residing inside node_modules (installed using npm)
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
     getDefaultProps : function() {
@@ -11,19 +13,27 @@ var ErrorModal = React.createClass({
         message : React.PropTypes.string.isRequired,
     },
     componentDidMount : function () {
+      var {title, message} = this.props;
+      var markup  = (
+          <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+              <h1>{title}</h1>
+              <p className="lead">An error occured while processing your request</p>
+              <p>{message} </p>
+              <button className="button" data-close="">
+                  OK
+              </button>
+          </div>
+      );
+
+      var $modal = $(ReactDOMServer.renderToString(markup));
+      $(ReactDOM.findDOMNode(this)).html($modal);
+
       var modal = new Foundation.Reveal($('#error-modal'));
       modal.open();
     },
     render: function () {
-        var {title, message} = this.props;
         return (
-            <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-                <h1>{title}</h1>
-                <p className="lead">An error occured while processing your request</p>
-                <p>{message} </p>
-                <button className="button" data-close="">
-                    OK
-                </button>
+            <div>
             </div>
         );
     }
